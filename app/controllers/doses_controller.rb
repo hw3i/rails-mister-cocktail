e2aa64c49725f0272f7266280c260e273edac011
Hwei Tan <chienhwei.tan@gmail.com>
2017-05-25 18:23:59 +0100

    Added form to the bottom of cocktails show

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,9 +2,6 @@ class DosesController < ApplicationController
   before_action :set_cocktail, only: [:new, :create]
   before_action :set_dose, only: [:destroy]
 
-  def new
-    @dose = Dose.new
-  end
 
   def create
     @dose = Dose.new(dose_params)
@@ -12,7 +9,7 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render "cocktails/show"
     end
   end
 
