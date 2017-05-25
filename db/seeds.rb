ee717f94cf2ea1ab9876af438049ee2837d74eea
Hwei Tan <chienhwei.tan@gmail.com>
2017-05-25 17:16:47 +0100

    New seed using JSON, added ingredients to index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-10.times do
-  Ingredient.create!(name: Faker::Food.spice)
+require 'open-uri'
+require 'json'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file = open(url).read
+data_hash = JSON.parse(file)
+
+data_hash['drinks'].each do |ingredient|
+  ingredient_name = ingredient['strIngredient1']
+  Ingredient.create!(name: ingredient_name)
 end
